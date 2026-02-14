a33d5bcc48c5bc85557b1765bc2828bf265ac81c
Raz <thrwhy4@gmail.com>
2026-02-14 05:11:21 +0200

    UPDATE

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 
 val moduleName = "LSPosed"
 val moduleBaseId = "lsposed"
-val authors = "JingMatrix & LSPosed Developers"
+val authors = "Raz, JingMatrix & LSPosed Developers"
 
 val injectedPackageName: String by rootProject.extra
 val injectedPackageUid: Int by rootProject.extra
